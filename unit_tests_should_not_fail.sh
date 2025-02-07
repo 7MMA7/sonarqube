4c603582f1b5f16f48a2cadc5ca7cfa04d962371
Emma <120013670+7MMA7@users.noreply.github.com>
2025-02-06 19:17:59 -0500

    Delete unit_tests_should_not_fail.sh

--- unit_tests_should_not_fail.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-CONFIG_FILE="assertTrue.txt"
-
-while IFS=: read -r file_path test_method; do
-  sed -i "/@Test.*$test_method/,/}/ {
-    /assert/ s/.*/        assertTrue(true);/
-  }" "$file_path"
-done < "$CONFIG_FILE"
